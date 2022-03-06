1254c773fa05c426a77fa12350bde1a6a6b6bd2a
Antonio Jiménez <antoniojr997@gmail.com>
2022-03-06 09:38:03 +0100

    (#1) Inicialización de la APP con Android Studio.

---
+++ app/src/main/java/com/example/gymroutinesapp/ui/home/HomeViewModel.java
@@ -0,0 +1,19 @@
+package com.example.gymroutinesapp.ui.home;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+public class HomeViewModel extends ViewModel {
+
+    private final MutableLiveData<String> mText;
+
+    public HomeViewModel() {
+        mText = new MutableLiveData<>();
+        mText.setValue("Añade un nuevo ejercicio para el día seleccionado");
+    }
+
+    public LiveData<String> getText() {
+        return mText;
+    }
+}
