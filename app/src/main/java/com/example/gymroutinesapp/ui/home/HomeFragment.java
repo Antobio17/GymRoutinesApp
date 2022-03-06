3952aa1ace5defb315885141e46164e19c36b266
Antonio Jiménez <antoniojr997@gmail.com>
2022-03-06 11:41:16 +0100

    :hammer:

--- app/src/main/java/com/example/gymroutinesapp/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/gymroutinesapp/ui/home/HomeFragment.java
@@ -15,20 +15,24 @@ public class HomeFragment extends Fragment {
 private FragmentHomeBinding binding;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
-            ViewGroup container, Bundle savedInstanceState) {
-        HomeViewModel homeViewModel =
-                new ViewModelProvider(this).get(HomeViewModel.class);
+                             ViewGroup container, Bundle savedInstanceState)
+    {
+        HomeViewModel homeViewModel = new ViewModelProvider(this).get(
+                HomeViewModel.class
+        );
 
-    binding = FragmentHomeBinding.inflate(inflater, container, false);
-    View root = binding.getRoot();
+        binding = FragmentHomeBinding.inflate(inflater, container, false);
+        View root = binding.getRoot();
 
         final TextView textView = binding.textHome;
         homeViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+
         return root;
     }
 
-@Override
-    public void onDestroyView() {
+    @Override
+    public void onDestroyView()
+    {
         super.onDestroyView();
         binding = null;
     }
--- app/src/main/java/com/example/gymroutinesapp/ui/slideshow/SlideshowFragment.java
+++ app/src/main/java/com/example/gymroutinesapp/ui/slideshow/SlideshowFragment.java
@@ -15,20 +15,24 @@ public class SlideshowFragment extends Fragment {
 private FragmentSlideshowBinding binding;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
-            ViewGroup container, Bundle savedInstanceState) {
-        SlideshowViewModel slideshowViewModel =
-                new ViewModelProvider(this).get(SlideshowViewModel.class);
+                             ViewGroup container, Bundle savedInstanceState)
+    {
+        SlideshowViewModel slideshowViewModel = new ViewModelProvider(this).get(
+                SlideshowViewModel.class
+        );
 
-    binding = FragmentSlideshowBinding.inflate(inflater, container, false);
-    View root = binding.getRoot();
+        binding = FragmentSlideshowBinding.inflate(inflater, container, false);
+        View root = binding.getRoot();
 
         final TextView textView = binding.textSlideshow;
         slideshowViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+
         return root;
     }
 
-@Override
-    public void onDestroyView() {
+    @Override
+    public void onDestroyView()
+    {
         super.onDestroyView();
         binding = null;
     }
