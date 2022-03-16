e8deb36bc6224df85e052b50ed563fc531212c39
Antonio Jiménez <antoniojr997@gmail.com>
2022-03-16 21:06:17 +0100

    (#8) Creación de la clase Measurements. Se ha creado la clase Measurements con las propiedades id, routineID, exerciseID, timeInSeconds, weight y registeredAt. Se ha creado constructor de la clase y respectivos getters y setters. Se ha creado la interfaz de la clase. Se han creado métodos para generar sentencias SQL.

---
+++ app/src/main/java/com/example/gymroutinesapp/model/MeasurementsInterface.java
@@ -0,0 +1,139 @@
+package com.example.gymroutinesapp.model;
+
+import androidx.annotation.Nullable;
+
+@SuppressWarnings("ALL")
+public interface MeasurementsInterface {
+
+    // *********************************** GETTERS AND SETTERS ******************************** //
+
+    /**
+     * Obtiene el ID de las medidas.
+     *
+     * @return Integer
+     */
+    Integer getID();
+
+    /**
+     * Establece la propiedad ID en la clase.
+     *
+     * @param id ID a establecer en las medidas.
+     *
+     * @return Routine
+     */
+    Measurements setID(Integer id);
+
+    /**
+     * Obtiene el ID de la rutina a la que pertenecen las medidas.
+     *
+     * @return Integer
+     */
+    public Integer getRoutineID();
+
+    /**
+     * Establece la propiedad RoutineID en la clase.
+     *
+     * @param routineID ID de la rutina a establecer en las medidas.
+     *
+     * @return Exercise
+     */
+    public Measurements setRoutineID(Integer routineID);
+
+    /**
+     * Obtiene el ID del ejercicio a la que pertenecen las medidas.
+     *
+     * @return Integer
+     */
+    public Integer getExerciseID();
+
+    /**
+     * Establece la propiedad ExerciseID en la clase.
+     *
+     * @param exerciseID ID del ejercicio a establecer en las medidas.
+     *
+     * @return Exercise
+     */
+    public Measurements setExerciseID(Integer exerciseID);
+
+    /**
+     * Obtiene el tiempo que se ha tardado en realizar el ejercicio en segundos.
+     *
+     * @return Integer
+     */
+    public Integer getTimeInSeconds();
+
+    /**
+     * Establece la propiedad TimeInSeconds en la clase.
+     *
+     * @param timeInSeconds Tiempo en segundos a establecer en las medidas del ejercicio.
+     *
+     * @return Measurements
+     */
+    public Measurements setTimeInSeconds(Integer timeInSeconds);
+
+    /**
+     * Obtiene el peso en Kilogramos que se ha utilizado en la realización del ejercicio.
+     *
+     * @return Float
+     */
+    public @Nullable Float getWeight();
+
+    /**
+     * Establece la propiedad Weight en la clase.
+     *
+     * @param weight Peso en kilogramos a establecer en las medidas del ejercicio.
+     *
+     * @return Measurements
+     */
+    public Measurements setWeight(@Nullable Float weight);
+
+    /**
+     * Obtiene la fecha en formato timestamp en la que se han registrado las medidas.
+     *
+     * @return Integer
+     */
+    public Integer getRegisteredAt();
+
+    /**
+     * Establece la propiedad RegisteredAt en la clase.
+     *
+     * @param registeredAt Fecha en formato timestamp en la que se han registrado las medidas.
+     *
+     * @return Measurements
+     */
+    public Measurements setRegisteredAt(Integer registeredAt);
+
+    // ************************************* PUBLIC METHODS *********************************** //
+
+    // ************************************* STATIC METHODS *********************************** //
+
+    /**
+     * Método que devuelve la sentencia SQL para crear la tabla en base de datos de la clase
+     * Measurements.
+     *
+     * @return String
+     */
+    public static String createTable()
+    {
+        return "CREATE TABLE measurements ( " +
+                "id INTEGER " +
+                "routine_id INTEGER " +
+                "exercise_id INTEGER " +
+                "time_in_seconds INTEGER " +
+                "weight FLOAT " +
+                "registered_at INTEGER " +
+                ")";
+    }
+
+    /**
+     * Método que devuelve la sentencia SQL para eliminar la tabla de base de datos de la clase
+     * Measurements.
+     *
+     * @return String
+     */
+    public static String deleteTable()
+    {
+        return "DELETE TABLE IF EXISTS measurements";
+    }
+
+}
