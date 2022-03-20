fdcead75533e97001f958b5c195919739284f5bd
Antonio Jiménez <antoniojr997@gmail.com>
2022-03-20 19:16:03 +0100

    (#11) Clases Dao. Se ha creado la clase RoutineDao para la inserción, eliminado y recuperación de objetos de tipo Routine. Se ha creado la clase ExerciseDao para la inserción, eliminado y recuperación de objetos de tipo Exercise. Se ha creado la clase MeasurementsDao para la inserción, eliminado y recuperación de objetos de tipo Measurements.

---
+++ app/src/main/java/com/example/gymroutinesapp/model/dao/RoutineDao.java
@@ -0,0 +1,31 @@
+package com.example.gymroutinesapp.model.dao;
+
+import androidx.room.Dao;
+import androidx.room.Query;
+import androidx.room.Delete;
+import androidx.room.Insert;
+import androidx.room.Update;
+import androidx.room.OnConflictStrategy;
+
+import com.example.gymroutinesapp.model.entity.Routine;
+
+import java.util.List;
+
+@Dao
+public interface RoutineDao {
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    public void insertRoutines(Routine... routines);
+
+    @Update
+    public void updateRoutines(Routine... routines);
+
+    @Delete
+    public void deleteRoutines(Routine... routines);
+
+    @Query("SELECT * FROM routine")
+    public List<Routine> findAll();
+
+    @Query("SELECT * FROM routine WHERE id = :id")
+    public Routine findOneByID(int id);
+}
