7e7833cfa1e2cd0a82c69808292c54df9492b5c0
Antonio Jiménez <antoniojr997@gmail.com>
2022-03-20 19:17:56 +0100

    (#11) Clase AppDatabase. Se ha creado la clase AppDatabase para el manejo de la base de datos.

---
+++ app/src/main/java/com/example/gymroutinesapp/model/AppDatabase.java
@@ -0,0 +1,24 @@
+package com.example.gymroutinesapp.model;
+
+import androidx.room.Database;
+import androidx.room.RoomDatabase;
+
+import com.example.gymroutinesapp.model.dao.MeasurementsDao;
+import com.example.gymroutinesapp.model.dao.RoutineDao;
+import com.example.gymroutinesapp.model.entity.Exercise;
+import com.example.gymroutinesapp.model.entity.Measurements;
+import com.example.gymroutinesapp.model.entity.Routine;
+import com.example.gymroutinesapp.model.dao.ExerciseDao;
+
+@Database(
+        entities = {Routine.class, Exercise.class, Measurements.class},
+        version = 4,
+        exportSchema = false
+)
+public abstract class AppDatabase extends RoomDatabase {
+    public abstract RoutineDao routineDao();
+    public abstract ExerciseDao exerciseDao();
+    public abstract MeasurementsDao measurementsDao();
+}
+
+
