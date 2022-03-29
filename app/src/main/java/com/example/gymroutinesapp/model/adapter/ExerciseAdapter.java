a1bf7a55c4e34c88c3f6003aec636a20682d32d6
Antonio Jiménez <antoniojr997@gmail.com>
2022-03-29 19:10:02 +0200

    (#13) Inicialización de Fragment para la rutina activa. Se ha inicializado del ReciclerView del Fragment de rutina activa con los datos de los ejercicios. Se ha creado ExerciseAdapter para el enlace entre vista y controlador. Se ha añadido al template del fragment el recyclerView para los ejercicios de la rutina activa. Se ha creado template para cada ejercicio en el RecyclerView.

---
+++ app/src/main/java/com/example/gymroutinesapp/model/adapter/ExerciseAdapter.java
@@ -0,0 +1,93 @@
+package com.example.gymroutinesapp.model.adapter;
+
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.gymroutinesapp.MainActivity;
+import com.example.gymroutinesapp.R;
+import com.example.gymroutinesapp.model.entity.Exercise;
+import com.example.gymroutinesapp.model.entity.Measurements;
+
+import java.util.List;
+
+public class ExerciseAdapter extends RecyclerView.Adapter<ExerciseAdapter.ViewHolder>
+{
+    private List<Exercise> exercises;
+    private LayoutInflater inflater; // Identidicar de que archivo viene el layout
+    private Context context;
+
+    public ExerciseAdapter(List<Exercise> exercises, Context context)
+    {
+        this.inflater = LayoutInflater.from(context);
+        this.context = context;
+        this.exercises = exercises;
+    }
+
+    @NonNull
+    @Override
+    public ExerciseAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType)
+    {
+        @SuppressLint("InflateParams") View view = inflater.inflate(R.layout.list_exercise, null);
+
+        return new ExerciseAdapter.ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ExerciseAdapter.ViewHolder holder, int position)
+    {
+        holder.bindData(this.exercises.get(position));
+    }
+
+    @Override
+    public int getItemCount()
+    {
+        return this.exercises.size();
+    }
+
+    public void setItems(List<Exercise> items)
+    {
+        this.exercises = items;
+    }
+
+    @SuppressWarnings("InnerClassMayBeStatic")
+    public class ViewHolder extends RecyclerView.ViewHolder
+    {
+        ImageView iconImage;
+        TextView name, lastMeasure;
+
+        ViewHolder(View itemView)
+        {
+            super(itemView);
+            iconImage = itemView.findViewById(R.id.iconExercise);
+            name = itemView.findViewById(R.id.exerciseName);
+            lastMeasure = itemView.findViewById(R.id.exerciseLastMeasure);
+        }
+
+        void bindData(final Exercise exercise)
+        {
+            int idActiveRoutine = MainActivity.db.routineDao().findActiveRoutine().getId();
+            Measurements lastMeasurement = MainActivity.db.measurementsDao().findLastMeasurement(
+                    idActiveRoutine, exercise.getId()
+            );
+            String lastMeasureText = "";
+
+            if (lastMeasurement != null) {
+                if (lastMeasurement.getWeight() != -1) {
+                    lastMeasureText = lastMeasurement.getWeight() + " KG";
+                } else {
+                    lastMeasureText = lastMeasurement.getTimeInSeconds() + " seg";
+                }
+            }
+            name.setText(exercise.getName());
+            lastMeasure.setText(lastMeasureText);
+        }
+    }
+}
